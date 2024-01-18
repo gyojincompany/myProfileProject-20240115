f255b11bea3827b184f7d006d6f921d4bc581f06
gyojincompany <luckyred@gmail.com>
2024-01-18 16:17:46 +0900

    글 삭제 기능 추가

--- src/main/java/com/gyojincompany/home/dao/BoardDao.java
+++ src/main/java/com/gyojincompany/home/dao/BoardDao.java
@@ -10,5 +10,6 @@ public interface BoardDao {
 	public void writeDao(String qbmid, String qbmname, String qbmemail, String qbtitle, String qbcontent);//게시판 글 쓰기
 	public QAboardDto contentViewDao(String qbnum);//글 번호로 해당 글의 레코드 반환
 	public void contentModifyDao(String qbnum, String qbtitle, String qbcontent);//글 수정
+	public void contentDeleteDao(String qbnum);//글 삭제
 
 }
